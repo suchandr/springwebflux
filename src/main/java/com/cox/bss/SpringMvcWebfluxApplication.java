94bb3a6479e5b34210ab96a3371848dd0884216b
suchandr <sudha.chandran@capgemini.com>
2019-03-29 18:31:53 +0530

    it commit

---
+++ src/main/java/com/cox/bss/SpringMvcWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.cox.bss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cox/bss/SpringMvcWebfluxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cox.bss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMvcWebfluxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
